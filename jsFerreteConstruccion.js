e789505908bca177437f8626e696af00c7794289
LeonelERey <leogonzalez.17.98@gmail.com>
2017-02-08 15:07:06 -0300

    Add files via upload

--- jsFerreteConstruccion.js
+++ jsFerreteConstruccion.js
@@ -6,13 +6,53 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largo;
+	var ancho;
+	var perimetro
+	var metrosdealambre;
+
+
+	largo=document.getElementById('Largo').value;
+	ancho=document.getElementById('Ancho').value;
+
+	largo=parseInt(largo);
+	ancho=parseInt(ancho);
+	perimetro=largo+ancho;
+	metrosdealambre=perimetro*2*3;
+
+	alert("nesecitara "+metrosdealambre+" metros de alambre para dar tres vueltas al Rectangulo");
+
 
 }
 function Circulo () 
 {
-	
+	var radio;
+	var perimetro;
+
+	radio=document.getElementById('Radio').value;
+	perimetro=2*3.14*radio*3;
+
+	alert("nesecitara "+perimetro+" metros de alambre para dar tres vueltas al perimetro");
+
 }
 function Materiales () 
 {
-	
+	var largo;
+	var ancho;
+	var cal;
+	var cemento;
+	var cantidaddecal;
+	var cantidaddecemento,
+
+	cal=2
+	cemento=3
+	largo=document.getElementById('Largo').value;
+	ancho=document.getElementById('Ancho').value;
+
+	cantidaddecal=cal*largo;
+	cantidaddecemento=cemento*ancho;
+
+	alert("nesecitara "+cantidaddecemento+" de cemento y "+cantidaddecal+" de cal, para este perimetro");
+
+
 }
